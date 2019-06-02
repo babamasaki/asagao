3d946ac34e52214c31f04078c0b1c47fa83d6216
babamasaki <0508baba@gmail.com>
2019-06-02 06:28:16 +0000

    chapter4 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+teble_names = %w(members)            # %w: 配列を作る。配列の要素はスペース区切りで指定する。式の展開はされない。
+teble_names.each do |table_name|
+	path = Rails.root.join("db/seeds", Rails.env, table_name + ".rb")     
+	# rails.root: appのルートパスを(users/taro/rails/asagao)を表すオブジェクト。pathnameクラスのオブジェクト。
+	# join: joinメソッドにディレクトリ名をいくつも渡せば、[/users/taro/rails/asagao/db/development/members.rb]の様にパスを組み立てれる。
+
+	if File.exist?(path)        #ファイルがあれが実行する。
+		puts "Creating #{table_name}..."
+		require path
+	end
+end
